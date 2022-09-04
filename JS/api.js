c9f72511115e165da8b5e043667d4f7dbf854c46
Hamres <75183238+Hamres@users.noreply.github.com>
2022-09-04 19:21:45 +0300

    updated code 3

--- JS/api.js
+++ JS/api.js
@@ -11,9 +11,47 @@ export const getStatsData = async (nickname) => {
 
     const dataId = await responseId.json()
 
+    //кол-во боев
     const battle = (document.querySelector('#BATTLE_PARAMS').textContent = dataId.data[accountId].statistics.all.battles)
 
-    const damage = (document.querySelector('#MIDDLE-DAMAGE_PARAMS').textContent = dataId.data[accountId].statistics.all.damage_dealt / battle)
+    //ср урон
+    const damage = (document.querySelector('#MIDDLE-DAMAGE_PARAMS').textContent = dataId.data[accountId].statistics.all.damage_dealt / battle) //toFixed ?
+
+    // wg рейтинг
+    const wgRating = (document.querySelector('#WG_PARAMS').textContent = dataId.data[accountId].global_rating)
+
+    // % побед
+    const win = dataId.data[accountId].statistics.all.wins
+
+    const winRating = (document.querySelector('#WIN_PARAMS').textContent = (win / battle) * 100) //toFixed ?
+
+    // WN8
+
+    //table of expected
+    const table = await fetch(`https://static.modxvm.com/wn8-data-exp/json/wn8exp.json`)
+
+    const dataTable = await table.json()
+
+    console.log(dataTable)
+
+    // const avgDmg = dataId.data[accountId].statistics.all.damage_dealt;
+    // const avgSpot = dataId.data[accountId].statistics.all.spotted;
+    // const avgFrag = dataId.data[accountId].statistics.all.frags;
+    // const avgDef = dataId.data[accountId].statistics.all.dropped_capture_points;
+    // const avgWinRate = win;
+    const expDmg = dataTable.data[100].expDamage
+    // const expSpot = //ожидаемое количество обнаруженных;
+    // const expFrag = //ожидаемое количество уничтоженных;
+    // const expDef = //ожидаемое количество очков защиты;
+    // const expWinRate = //ожидаемое количество побед;
+
+    //   const expDmgArray = for (i in dataTable.data) {
+    //     x += "<h1>" + dataTable.data[i].expDamage + "</h1>";
+    // }
+
+    // WN8
+
+    console.log(expDmg)
 
     console.log(dataId)
   } catch (error) {
