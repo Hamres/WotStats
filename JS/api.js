6dd34b5ffd78d156aa5d80206d9e3d259fd63e15
Hamres <75183238+Hamres@users.noreply.github.com>
2022-09-25 18:29:03 +0300

    pre-release

--- JS/api.js
+++ JS/api.js
@@ -1,59 +1,159 @@
 export const getStatsData = async (nickname) => {
   try {
     // ищем по нику id аккаунта
+
     const response = await fetch(`https://api.worldoftanks.ru/wot/account/list/?application_id=092ce98e1d0e4584d8c87eb08d4b529a&search=${nickname}`)
 
     const data = await response.json()
+
     // ключ значение Id аккаунта
+
     const accountId = data.data['0'].account_id
+
     // выводим статистику по id аккаунта
+
     const responseId = await fetch(`https://api.worldoftanks.ru/wot/account/info/?application_id=092ce98e1d0e4584d8c87eb08d4b529a&account_id=${accountId}`)
 
     const dataId = await responseId.json()
 
+    // технка на аккаунте
+
+    // const tanks = await fetch(`https://api.worldoftanks.ru/wot/account/tanks/?application_id=092ce98e1d0e4584d8c87eb08d4b529a&account_id=${accountId}`)
+
+    // const tanksData = await tanks.json()
+
+    // console.log(tanksData)
+
     //кол-во боев
+
     const battle = (document.querySelector('#BATTLE_PARAMS').textContent = dataId.data[accountId].statistics.all.battles)
 
-    //ср урон
-    const damage = (document.querySelector('#MIDDLE-DAMAGE_PARAMS').textContent = dataId.data[accountId].statistics.all.damage_dealt / battle) //toFixed ?
+    //ср. урон
+
+    const middleDamageParams = document.querySelector('#MIDDLE-DAMAGE_PARAMS')
+    const DamageRating = dataId.data[accountId].statistics.all.damage_dealt / battle
+    middleDamageParams.textContent = DamageRating.toFixed()
 
     // wg рейтинг
+
     const wgRating = (document.querySelector('#WG_PARAMS').textContent = dataId.data[accountId].global_rating)
 
     // % побед
     const win = dataId.data[accountId].statistics.all.wins
 
-    const winRating = (document.querySelector('#WIN_PARAMS').textContent = (win / battle) * 100) //toFixed ?
+    const winParams = document.querySelector('#WIN_PARAMS')
+    const rating = (win / battle) * 100
+    winParams.textContent = rating.toFixed(2) + ' %'
 
-    // WN8
+    //==покрас==
 
-    //table of expected
-    const table = await fetch(`https://static.modxvm.com/wn8-data-exp/json/wn8exp.json`)
+    //бои
 
-    const dataTable = await table.json()
+    const battleId = dataId.data[accountId].statistics.all.battles
+    const elemBattle = document.querySelector('#BATTLE_PARAMS')
+    let colorClass = ''
+    const getColor = () => {
+      if (battleId > 1 && battleId < 4999) {
+        colorClass = 'red'
+      }
+      if (battleId >= 5000 && battleId < 8999) {
+        colorClass = 'orange'
+      }
+      if (battleId >= 9000 && battleId < 13999) {
+        colorClass = 'green'
+      }
+      if (battleId >= 14000 && battleId < 49999) {
+        colorClass = 'blue'
+      }
+      if (battleId >= 50000) {
+        colorClass = 'purple'
+      }
+      elemBattle.classList.add(colorClass)
 
-    console.log(dataTable)
+      return colorClass
+    }
 
-    // const avgDmg = dataId.data[accountId].statistics.all.damage_dealt;
-    // const avgSpot = dataId.data[accountId].statistics.all.spotted;
-    // const avgFrag = dataId.data[accountId].statistics.all.frags;
-    // const avgDef = dataId.data[accountId].statistics.all.dropped_capture_points;
-    // const avgWinRate = win;
-    const expDmg = dataTable.data[100].expDamage
-    // const expSpot = //ожидаемое количество обнаруженных;
-    // const expFrag = //ожидаемое количество уничтоженных;
-    // const expDef = //ожидаемое количество очков защиты;
-    // const expWinRate = //ожидаемое количество побед;
+    getColor()
 
-    //   const expDmgArray = for (i in dataTable.data) {
-    //     x += "<h1>" + dataTable.data[i].expDamage + "</h1>";
-    // }
+    //рейтинг WG
 
-    // WN8
+    const wgId = dataId.data[accountId].global_rating
+    const elemWg = document.querySelector('#WG_PARAMS')
+    let colorClassWg = ''
+    const getColorWg = () => {
+      if (wgId > 1 && wgId < 4830) {
+        colorClassWg = 'red'
+      }
+      if (wgId >= 4831 && wgId < 6919) {
+        colorClassWg = 'orange'
+      }
+      if (wgId >= 6920 && wgId < 9175) {
+        colorClassWg = 'green'
+      }
+      if (wgId >= 9176 && wgId < 10619) {
+        colorClassWg = 'blue'
+      }
+      if (wgId >= 10620) {
+        colorClassWg = 'purple'
+      }
+      elemWg.classList.add(colorClassWg)
 
-    console.log(expDmg)
+      return colorClassWg
+    }
 
-    console.log(dataId)
+    getColorWg()
+
+    // % побед
+
+    let winRating = rating
+    let colorClassWin = ''
+    const getColorWin = () => {
+      if (winRating > 0 && winRating < 46.49) {
+        colorClassWin = 'red'
+      }
+      if (winRating >= 46.5 && winRating < 52.49) {
+        colorClassWin = 'orange'
+      }
+      if (winRating >= 52.5 && winRating < 57.49) {
+        colorClassWin = 'green'
+      }
+      if (winRating >= 57.5 && winRating < 63.49) {
+        colorClassWin = 'blue'
+      }
+      if (winRating >= 63.5) {
+        colorClassWin = 'purple'
+      }
+      winParams.classList.add(colorClassWin)
+
+      return colorClassWin
+    }
+
+    getColorWin()
+
+    // ср.урон
+    let colorClassDamage = ''
+    const getColorDamage = () => {
+      if (DamageRating > 0 && DamageRating < 749) {
+        colorClassDamage = 'red'
+      }
+      if (DamageRating >= 750 && DamageRating < 999) {
+        colorClassDamage = 'orange'
+      }
+      if (DamageRating >= 1000 && DamageRating < 1799) {
+        colorClassDamage = 'green'
+      }
+      if (DamageRating >= 1800 && DamageRating < 2499) {
+        colorClassDamage = 'blue'
+      }
+      if (DamageRating >= 2500) {
+        colorClassDamage = 'purple'
+      }
+      middleDamageParams.classList.add(colorClassDamage)
+
+      return colorClassDamage
+    }
+
+    getColorDamage()
   } catch (error) {
     console.error(error)
   }
--- JS/index.js
+++ JS/index.js
@@ -5,9 +5,31 @@ const selectWhat = document.querySelector('#select_what')
 const btn = document.querySelector('#button')
 const errorBlock = document.querySelector('#errorBlock')
 
+let resetColorBattle = document.querySelector('#BATTLE_PARAMS')
+let resetColorWg = document.querySelector('#WG_PARAMS')
+let resetColorWin = document.querySelector('#WIN_PARAMS')
+let resetColorDamage = document.querySelector('#MIDDLE-DAMAGE_PARAMS')
+
 //прослушка на кнопку
 form.addEventListener('submit', (event) => {
   event.preventDefault()
+
+  const classListBattle = resetColorBattle.classList
+  while (classListBattle.length > 1) {
+    classListBattle.remove(classListBattle.item(1))
+  }
+  const classListsWg = resetColorWg.classList
+  while (classListsWg.length > 1) {
+    classListsWg.remove(classListsWg.item(1))
+  }
+  const classListsWin = resetColorWin.classList
+  while (classListsWin.length > 1) {
+    classListsWin.remove(classListsWin.item(1))
+  }
+  const classListsDamage = resetColorDamage.classList
+  while (classListsDamage.length > 1) {
+    classListsDamage.remove(classListsDamage.item(1))
+  }
 
   const whatText = selectWhat.value
 })
