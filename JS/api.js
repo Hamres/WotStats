b5d4df1f3f7ba412b8eb6b0acd0aefef9a4521f5
Hamres <75183238+Hamres@users.noreply.github.com>
2022-11-05 09:31:23 +0300

    update for lesta API

--- JS/api.js
+++ JS/api.js
@@ -2,7 +2,7 @@ export const getStatsData = async (nickname) => {
   try {
     // ищем по нику id аккаунта
 
-    const response = await fetch(`https://api.worldoftanks.ru/wot/account/list/?application_id=092ce98e1d0e4584d8c87eb08d4b529a&search=${nickname}`)
+    const response = await fetch(`https://api.tanki.su/wot/account/list/?application_id=092ce98e1d0e4584d8c87eb08d4b529a&search=${nickname}`)
 
     const data = await response.json()
 
@@ -12,7 +12,7 @@ export const getStatsData = async (nickname) => {
 
     // выводим статистику по id аккаунта
 
-    const responseId = await fetch(`https://api.worldoftanks.ru/wot/account/info/?application_id=092ce98e1d0e4584d8c87eb08d4b529a&account_id=${accountId}`)
+    const responseId = await fetch(`https://api.tanki.su/wot/account/info/?application_id=092ce98e1d0e4584d8c87eb08d4b529a&account_id=${accountId}`)
 
     const dataId = await responseId.json()
 
